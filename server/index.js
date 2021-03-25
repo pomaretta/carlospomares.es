850548ec349c23d545c511113e76e74bbe5eeafd
pomaretta <cpomaresp@gmail.com>
2021-03-25 21:20:00 +0100

    Making database

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 const express = require('express')
+const { promisify } = require('util')
+const mysql = require('mysql')
+const credentials = require('./env/env')
 
 const PORT = {
     dev: 8000,
     build: 3000
 }
 
+// EXPRESS
 const server = express()
 
+// DATABASE
+const pool = mysql.createPool(credentials)
+const query = promisify(pool.query).bind(pool)
+
+const getProjects = async () => {
+    const projects = await query("SELECT * FROM projects")
+    return projects
+}
+
+// GET
 server.get('/projects', (req,res) => {
-    res.json({status:200})
+    getProjects().then(r => res.send(r))
 })
 
+// LISTEN
 server.listen(PORT.dev, () => {
     console.log(`SERVER Listening on port ${PORT.dev}`)
 })
