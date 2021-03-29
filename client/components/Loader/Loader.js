219f7e469313ce30425d1fe800679fd2d6c75315
pomaretta <cpomaresp@gmail.com>
2021-03-29 19:08:31 +0200

    Working on loader

---
+++ client/components/Loader/Loader.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+class Loader extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
