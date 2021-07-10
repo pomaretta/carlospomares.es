6b73be435a9bb8ab0541daf0c823773aa81cf8fd
Carlos Pomares <cpomaresp@gmail.com>
2021-07-10 12:27:45 +0200

    Removed HTTPS

--- api/index.js
+++ api/index.js
@@ -4,18 +4,14 @@
 
 // DEPENDENCIES
 import express from 'express'
-import https from 'https'
 import cors from 'cors'
-import fs from 'fs'
 import compression from 'compression'
 import helmet from 'helmet'
 
 // CONFIG
 const CONFIG = {
     hostname: process.env.HOSTNAME,
-    port: process.env.PORT,
-    key: process.env.CERT_KEY,
-    cert: process.env.CERT_SECRET
+    port: process.env.PORT
 }
 
 // INIT
@@ -36,30 +32,12 @@ import mail from './routes/mail.js' // MAIL ROUTES
 api.use(mail)
 
 // LISTEN
-
-if (process.env.PROTOCOL == "https") {
-    https.createServer(
-        {
-            key: fs.readFileSync(CONFIG.key),
-            cert: fs.readFileSync(CONFIG.cert)
-        },api
-    ).listen(
-        CONFIG.port,
-        CONFIG.hostname,
-        () => {
-            console.log(
-                `API Listening on Hostname=${CONFIG.hostname}, Port=${CONFIG.port} over HTTPS, PRODUCTION=${process.env.NODE_ENV == 'production'}`
-            )
-        }
-    )
-} else {
-    api.listen(
-        CONFIG.port,
-        CONFIG.hostname,
-        () => {
-            console.log(
-                `API Listening on Hostname=${CONFIG.hostname}, Port=${CONFIG.port} over HTTP, PRODUCTION=${process.env.NODE_ENV == 'production'}`
-            )
-        }
-    )
-}
+api.listen(
+    CONFIG.port,
+    CONFIG.hostname,
+    () => {
+        console.log(
+            `API Listening on Hostname=${CONFIG.hostname}, Port=${CONFIG.port} over HTTP, PRODUCTION=${process.env.NODE_ENV == 'production'}`
+        )
+    }
+)
